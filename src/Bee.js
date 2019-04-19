63e09bee0f3bc6cbf6c0bddcebc520ff7b5109a5
Chris Huang <chrishuang128@gmail.com>
2019-04-19 09:08:05 -0700

    Finished all 6ees inheritence

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,9 @@
-class Bee {
-  // TODO..
+class Bee extends Grub {
+  constructor (){
+    super();
+
+    this.job = 'Keep on growing'
+    this.color = 'yellow'
+    this.age = 5;
+  }
 };
--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,12 @@
-class ForagerBee {
+class ForagerBee extends Bee{
   // TODO..
+  constructor(){
+    super();
+    this.canFly = true;
+    this.age = 10;
+    this.job = 'find pollen';
+    this.color = 'yellow';
+    this.treasureChest = [];
+  }
+  forage = () => this.treasureChest.push('treasure');
 };
